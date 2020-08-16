2df08a00a2fd82a81609617eccd9b875f4fd83fb
risanomura110 <aiueo110boy@gmail.com>
2020-08-16 23:25:54 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root "rooms#index"#http://topページを表示させる
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
 
+  end
 end
